8b96b22245c849be09492060fc9ac735856bf2c7
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-11-16 13:27:01 +0900

    Added additional test for md-checkbox

--- tests/widgets/test_mdcheckbox.py
+++ tests/widgets/test_mdcheckbox.py
@@ -22,10 +22,46 @@ class SimpleMDCheckBoxTest(TestCase):
         self.widget = MDCheckBox(self.label)
 
     def test_render(self):
-        """Test render invocation."""
+        """Test render invokation."""
         result = self.widget.render("result", None)
         data = (
             "<md-checkbox name=\"result\" type=\"checkbox\">"
             "%s</md-checkbox>"
+        ) % self.label
+        self.assertEqual(result, data)
+
+
+class MDCheckBoxCheckedTest(TestCase):
+    """MDCheckBox Checked test."""
+
+    def setUp(self):
+        """Setup."""
+        self.label = "This is a test"
+        self.widget = MDCheckBox(self.label)
+
+    def test_render(self):
+        """Test render invokation."""
+        result = self.widget.render("result", True)
+        data = (
+            "<md-checkbox checked=\"checked\" name=\"result\" "
+            "type=\"checkbox\">%s</md-checkbox>"
+        ) % self.label
+        self.assertEqual(result, data)
+
+
+class MDCheckBoxHasClassTest(TestCase):
+    """MDCheckBox test in the case that the widget has a value."""
+
+    def setUp(self):
+        """Setup."""
+        self.label = "This is a test"
+        self.widget = MDCheckBox(self.label)
+
+    def test_render(self):
+        """Test render invokation."""
+        result = self.widget.render("result", "UWAAAAAHHH")
+        data = (
+            "<md-checkbox checked=\"checked\" name=\"result\" "
+            "type=\"checkbox\" value=\"UWAAAAAHHH\">%s</md-checkbox>"
         ) % self.label
         self.assertEqual(result, data)
